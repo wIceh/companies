e39b1ce1b252dbd4c825b5cf642f1d240fba848f
Greezlyyy <yuri.bevilacqua866@gmail.com>
2024-09-20 10:59:18 +0200

    Initial commit

---
+++ src/main/java/me/wiceh/companies/Companies.java
@@ -0,0 +1,17 @@
+package me.wiceh.companies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Companies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/wiceh/companies/objects/Company.java
@@ -0,0 +1,4 @@
+package me.wiceh.companies.objects;
+
+public class Company {
+}
